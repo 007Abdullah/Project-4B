00d9e3755ce39292bd0b890c634728b8a53a14d1
007Abdullah <kb337137@gmail.com>
2021-06-17 03:49:14 +0500

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({
+  main_div: {
+    width: '100%',
+    height: '900px',
+    display: 'flex',
+    backgroundColor: 'red'
+
+  }
+}))
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className={classes.main_div}>
+
+      </div>
+    </React.Fragment>
   );
 }
 
